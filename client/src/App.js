de8219e94d84cd141db88702bca644d0ac1b91ef
Nicholas Juliana <naj1243@gmail.com>
2023-08-10 21:14:01 -0400

    added more to dash

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,11 @@ function App() {
           <Route path='/' element={<Register/>}/>
           <Route path='/login' element={<Login/>}/>
           <Route path='/dashboard' element={<Dashboard/>}/>
+          {/* will need path for see single review - /tender/review */}
+          {/* will need path for edit/update review */}
+          {/* will need path for edit/update user */}
+          {/* will need path for delete tender review */}
+          {/* will need path for delete user */}
         </Routes>
       </BrowserRouter>
     </div>
--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,8 +1,9 @@
-import React, {useState} from 'react';
+import React, {useEffect,useState} from 'react';
 import axios from 'axios';
-import {useNavigate} from 'react-router-dom'
+import {useNavigate, Link} from 'react-router-dom'
 
 const Dashboard = (props) => {
+  const [allReviews, setAllReviews] = useState([]);
   const navigate = useNavigate()
   const Logout = () => {
     axios.post('http://localhost:8000/api/logout', {}, {withCredentials:true})
@@ -13,11 +14,51 @@ const Dashboard = (props) => {
         console.log(err);
       })
   }
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8000/review`)
+      .then((res) => {
+        console.log(res.data);
+        setAllReviews(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+        console.log(err.response);
+      });
+  }, []);
+
   return(
     <div>
-      <h1>Welcome to the Dashboard</h1>
       <div>
+        <h1>Welcome to the Dashboard</h1>
         <button onClick={Logout}>logout</button>
+      </div>
+      <div className="mainDisplay">
+        <h1 className="dashHeader">Your Chicken Tenders</h1>
+        {allReviews.map((review,index) => {
+        return(
+          <div className="displayContainer" key={review._id}>
+            <div className="displayHeader">
+              {/* <Link to={`/chickentinder/tender/${review._id}`}> */}
+              <p>{review.restaurantName}</p>
+              {/* </Link> */}
+              <p>Price: {review.price}</p>
+            </div>
+            <div className="displayBody">
+              <p>Flavor: {review.flavor}</p>
+              <p>Crispiness: {review.crispiness}</p>
+              <p>Size: {review.size}</p>
+              <p>Comment: {review.comments}</p>
+            </div>
+          </div>)
+        })}
+        <div>
+          <button id="button">
+            {/* <Link to={`/chickentinder/new`}> */}
+              Add a Tender
+            {/* </Link> */}
+          </button>
+        </div>
       </div>
     </div>
   )
