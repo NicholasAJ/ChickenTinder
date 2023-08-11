959d5efa97a678453f5bfc7f2cc6a6b526005acd
Nicholas Juliana <naj1243@gmail.com>
2023-08-10 20:57:05 -0400

    fixed logout function

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -2,19 +2,17 @@ import React, {useState} from 'react';
 import axios from 'axios';
 import {useNavigate} from 'react-router-dom'
 
-const Logout = () => {
-  const navigate = useNavigate();
-  axios.post('http://localhost:8000/api/logout', {}, {withCredentials:true})
-    .then((res) => {
-      console.log(res)
-      navigate('/')
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-}
-
 const Dashboard = (props) => {
+  const navigate = useNavigate()
+  const Logout = () => {
+    axios.post('http://localhost:8000/api/logout', {}, {withCredentials:true})
+      .then((res) => {
+        navigate('/')
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
   return(
     <div>
       <h1>Welcome to the Dashboard</h1>
