3eb459cad8db1d383b4ae0c1f14c7f11ac9151c5
Nicholas Juliana <naj1243@gmail.com>
2023-08-14 08:03:32 -0400

    updating push

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Header = ({ user }) => {
 
@@ -7,6 +8,8 @@ const Header = ({ user }) => {
       // all the logged in links
       <div>
         <h1>Chicken Tinder</h1>
+        <Link to='/'>Home</Link>
+        <Link to='/logout'>Log Out</Link>
       </div>
     )
   } else {
@@ -14,6 +17,7 @@ const Header = ({ user }) => {
       //not logged in links
       <div>
         <h1>Chicken Tinder</h1>
+        <p>Track your tendies</p>
       </div>
     )
   }
